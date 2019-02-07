01ac8d7851cc316067fbfa593a035e008c462f8b
Victor Munoz <Vmunoz94@hotmail.com>
2019-02-06 20:27:57 -0800

    functions complete

--- node/functions.js
+++ node/functions.js
@@ -10,10 +10,20 @@ const arrow = (test) => {
   console.log(test);
 }
 
-arrow('hi')
+arrow('hi');
 
 // for practice
 
 // make an arrow function with no block body
+const myArrow = (y) => y + 1;
+console.log(myArrow(1));
 
 // make a function that returns a function
+const functionFactory = () => () => console.log('This is wierd');
+const a = functionFactory();
+a(); 
+
+const anotherFunction = functionFactory;
+anotherFunction()();
+
+functionFactory()();
